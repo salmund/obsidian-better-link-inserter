5c38ffda4a2099c9903050d857405be86b88f299
Louis <105465034+salmund@users.noreply.github.com>
2023-08-30 13:51:16 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,32 @@
-import {
-	Editor,
-	MarkdownView,
-	Plugin,
-} from "obsidian";
-
-
+import { Editor, MarkdownView, Plugin } from "obsidian";
 
 export default class BetterLinkInserterPlugin extends Plugin {
 	async onload() {
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: "use-selected-word-as-alias",
 			name: "Insert an internal link (using selected word as alias if possible)",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const selected_word = editor.getSelection();
-				if (selected_word == "") {
-					editor.replaceSelection(`[[]]`);
-					const current_line = editor.getCursor();
-					const wanted_position = current_line;
-					wanted_position.ch =
-						wanted_position.ch - 2;
-					editor.setCursor(wanted_position);
-				} else {
-					editor.replaceSelection(`[[|${selected_word}]]`);
-					const current_line = editor.getCursor();
-					const wanted_position = current_line;
-					wanted_position.ch =
-						wanted_position.ch - (selected_word.length + 3);
-					editor.setCursor(wanted_position);
-				}
-			},
+			editorCallback: this.insertInternalLinkWithAlias,
 		});
-
 	}
 
 	onunload() {}
+
+	private insertInternalLinkWithAlias = (editor: Editor, view: MarkdownView) => {
+		const selectedWord = editor.getSelection();
+		const hasSelectedWord = selectedWord !== "";
+
+		const linkText = hasSelectedWord ? `|${selectedWord}` : "";
+		const cursorOffset = hasSelectedWord ? 3 + selectedWord.length : 2;
+
+		this.replaceSelectionAndMoveCursor(editor, `[[${linkText}]]`, cursorOffset);
+	};
+
+	private replaceSelectionAndMoveCursor = (editor: Editor, text: string, cursorOffset: number) => {
+		editor.replaceSelection(text);
+
+		const cursorPosition = editor.getCursor();
+		cursorPosition.ch -= cursorOffset;
+
+		editor.setCursor(cursorPosition);
+	};
 }
